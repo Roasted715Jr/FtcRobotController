153d31b512877619cd1842f7b9999d50beafde86
Matthew Duphily <matthew.duphily@live.com>
2024-10-17 01:28:44 -0400

    Performed preliminary tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -58,19 +58,26 @@ public final class MecanumDrive {
         // TODO: fill in these values based on
         //   see https://ftc-docs.firstinspires.org/en/latest/programming_resources/imu/imu.html?highlight=imu#physical-hub-mounting
         public RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection =
-                RevHubOrientationOnRobot.LogoFacingDirection.UP;
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
         public RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection =
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
         // drive model parameters
-        public double inPerTick = 1;
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 0;
+//        public double inPerTick = 47.375 / 15955; //0.00296928862425571921
+        public double inPerTick = 94.5 / 31938.5; //0.0029588114657858071
+//        public double lateralInPerTick = inPerTick;
+//        public double lateralInPerTick = 0.002326369568479737; //Not close enough to use inPerTick
+        public double lateralInPerTick = 0.002271970714217727; //Not close enough to use inPerTick
+//        public double trackWidthTicks = 4905.027855288778;
+        public double trackWidthTicks = 4845.632923606017;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
-        public double kA = 0;
+//        public double kS = 0.9489120533237778; //http://192.168.43.1:8080/tuning/forward-ramp.html
+        public double kS = 0.895535313487565; //http://192.168.43.1:8080/tuning/forward-ramp.html
+//        public double kV = 0.0005730362145942904; //http://192.168.43.1:8080/tuning/forward-ramp.html
+        public double kV = 0.0005894893311299001; //http://192.168.43.1:8080/tuning/forward-ramp.html
+//        public double kA = 0.0001; //0.0000001
+        public double kA = 0.000001; //0.0000001
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,13 +89,13 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
+        public double axialGain = 2.0;
+        public double lateralGain = 2.0;
         public double headingGain = 0.0; // shared with turn
 
-        public double axialVelGain = 0.0;
-        public double lateralVelGain = 0.0;
-        public double headingVelGain = 0.0; // shared with turn
+        public double axialVelGain = 1.0;
+        public double lateralVelGain = 1.0;
+        public double headingVelGain = 2.0; // shared with turn
     }
 
     public static Params PARAMS = new Params();
@@ -228,7 +235,8 @@ public final class MecanumDrive {
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // TODO: reverse motor directions if needed
-        //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
@@ -237,7 +245,8 @@ public final class MecanumDrive {
 
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
-        localizer = new DriveLocalizer();
+//        localizer = new DriveLocalizer();
+        localizer = new ThreeDeadWheelLocalizer(hardwareMap, PARAMS.inPerTick);
 
         FlightRecorder.write("MECANUM_PARAMS", PARAMS);
     }
